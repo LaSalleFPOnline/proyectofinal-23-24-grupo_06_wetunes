0e07b72491be224fe7dcf458e4974ea6b5ecb82f
ericxc <eric.xc@hotmail.com>
2024-03-27 12:00:11 +0100

    Initial commit

---
+++ src/app/test-page/test-page.page.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { Song, SongsService } from '../services/songs.service';
+
+@Component({
+  selector: 'app-test-page',
+  templateUrl: './test-page.page.html',
+  styleUrls: ['./test-page.page.scss'],
+  standalone: true,
+  imports: [IonicModule, CommonModule, FormsModule]
+})
+export class TestPagePage implements OnInit {
+
+//Aquí simulamos mostrar las canciones
+
+songs: Song[] = []
+
+productsService = inject(SongsService);
+
+  async ngOnInit() {
+    const response = await this.productsService.getAll();
+    this.songs = response.results;
+  }
+
+}
