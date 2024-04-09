86655984fab97a2acfa95cf842a35fefe1101828
Harman2301 <g.harman2003@gmail.com>
2024-04-09 19:16:20 +0200

    Changes in login 09-04-24

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -2,25 +2,41 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-import { RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule, RouterLink]
+  imports: [IonicModule, CommonModule, FormsModule]
 })
 export class LoginPage implements OnInit {
+  user = { email: '', password: '' };
+  emailError = false;
+  showPassword = false;
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
-  showPassword = false;
 
   togglePassword() {
     this.showPassword = !this.showPassword;
   }
 
+  onLogin() {
+    if (!this.validateEmail(this.user.email)) {
+      this.emailError = true;
+    } else {
+      this.emailError = false;
+      // Navigate only if the email is valid
+      this.router.navigate(['/test-page']);
+    }
+  }
+
+  validateEmail(email: string): boolean {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  }
 }
