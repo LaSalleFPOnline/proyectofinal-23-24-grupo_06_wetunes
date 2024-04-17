e8f86987d503e262c03e6f720c47a18751088d5f
ericxc <eric.xc@hotmail.com>
2024-04-17 19:52:02 +0200

    añadidas credenciales firebase

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { Router } from '@angular/router';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -12,11 +14,15 @@ import { Router } from '@angular/router';
   imports: [IonicModule, CommonModule, FormsModule]
 })
 export class LoginPage implements OnInit {
+
+  public email: any;
+  public password: any;
+
   user = { email: '', password: '' };
   emailError = false;
   showPassword = false;
 
-  constructor(private router: Router) { }
+  constructor(public fireAuth: AngularFireAuth, public router: Router, public toastController: ToastController) { }
 
   ngOnInit() {
   }
@@ -33,7 +39,31 @@ export class LoginPage implements OnInit {
       this.emailError = false;
       // Navigate only if the email is valid
 
-      /* this.fireAuth.signInWithEmailAndPassword(this.email, this.password) */
+      this.fireAuth.signInWithEmailAndPassword(this.email, this.password)
+      .then(async res => {
+        
+        let nombreUsur: string = this.email.split('@')[0]; //Separamos el nombre del usuario del mail
+        localStorage.setItem('usuario', nombreUsur); //Guardamos el nombre del usuario en el Storage para poderlo usar desde otras pages
+
+        this.router.navigate(['./home/'+ nombreUsur]); //Lo añadimos a la url configurando tambien el app-routing
+
+    })
+    .catch(async error => {
+        this.email="";
+        this.password="";
+
+        const toast = await this.toastController.create({
+          message: "Email o Password no validos",
+          duration: 3000,
+          position: 'top'
+        })
+        toast.present();
+    })
+
+
+
+
+
       this.router.navigate(['home']);
     }
   }
