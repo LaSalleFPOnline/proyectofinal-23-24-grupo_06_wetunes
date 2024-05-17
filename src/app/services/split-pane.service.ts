9a407a2498bd55f774a28fa256901c5b8f67b617
Belen Ferreras <belenmaria2@msn.com>
2024-05-17 18:59:44 +0200

    Codigo comentado y formateado

--- src/app/services/split-pane.service.ts
+++ src/app/services/split-pane.service.ts
@@ -11,11 +11,16 @@ export class SplitPaneService {
 
   constructor(public platform: Platform, private authService: AuthService) {
     this.splitPaneState = false;
-   }
+  }
 
-   getSplitPane(){
-    if(this.authService.getAuthLogin()){ //Si tenemos usuario significa que estamos en cualquier pagina menos la de login
-      if(this.platform.width() > 850){ //Ya que si es menor no queremos que se muestre pq queremos el menú hamburguesa
+  /**
+   * Método que determina el estado de visibilidad del Split Pane (menú lateral) basado en el estado de autenticación del usuario 
+   * y el ancho de la plataforma.
+   * @returns boolean Indica si el Split Pane debe estar visible o no.
+   */
+  getSplitPane() {
+    if (this.authService.getAuthLogin()) { //Si tenemos usuario significa que estamos en cualquier pagina menos la de login
+      if (this.platform.width() > 850) { //Ya que si es menor no queremos que se muestre pq queremos el menú hamburguesa
         this.splitPaneState = true;
       } else {
         this.splitPaneState = false;
@@ -24,7 +29,6 @@ export class SplitPaneService {
       this.splitPaneState = false;
     }
     return this.splitPaneState;
-    
   }
 
 }
